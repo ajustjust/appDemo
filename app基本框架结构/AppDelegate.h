b4b6766940bf94d3d209b2f9bcc98aedb504eeb9
杨华 <593283083@qq.com>
2015-08-13 15:30:30 +0800

    Initial Commit

---
+++ app基本框架结构/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app基本框架结构
+//
+//  Created by xxzx on 15/8/13.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app基本框架结构/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  app基本框架结构
+//
+//  Created by xxzx on 15/8/13.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
